11c0a4845f6155efc2a1f502c4dd8514ccf6a242
Caitlin Rafael <77552533+grapes-cr@users.noreply.github.com>
2022-01-11 14:51:16 -0800

    Uploaded rails work

---
+++ db/migrate/20220111221952_create_shops.rb
@@ -0,0 +1,11 @@
+class CreateShops < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shops do |t|
+      t.string :name
+      t.integer :shelves
+      t.string :unshelvedCandies
+
+      t.timestamps
+    end
+  end
+end
